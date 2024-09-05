0b863f1d07f04511c7e82931eb17aff7084d23b2
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-09-05 13:59:34 -0400

    update

--- miApp001/src/app/siete/siete.page.ts
+++ miApp001/src/app/siete/siete.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-siete',
@@ -11,12 +12,31 @@ export class SietePage implements OnInit {
   primerApellido:string;
   n1: number = 0;
   n2: number = 0;
-
-  constructor() { 
+  resultado : number = 0;
+  constructor(private toast: ToastController) { 
     this.primerApellido = "Fernandez";
   }
 
   ngOnInit() {
   }
+  async sumar()
+  {
+    this.resultado = this.n1 + this.n2;
 
+    const mensaje = await this.toast.create({
+      message : 'El resultado de la suma es: ' + this.resultado,
+      duration: 2000,
+      position: 'bottom', // top y middle
+    });
+    await mensaje.present();
+
+
+  }
+  limpiar()
+  {
+    this.resultado = this.n1 = this.n2 = 0;
+  }
+    // Ejercicio 9:
+    // Crear las operaciones aritmeticas *,/ y -, 
+    // validando los numero enviado mensaje adecuado
 }
