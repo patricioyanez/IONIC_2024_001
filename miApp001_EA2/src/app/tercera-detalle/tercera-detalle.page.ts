a1cfadc07d277b437450fb13c946ef3506afc199
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-03 13:10:47 -0300

    update

---
+++ miApp001_EA2/src/app/tercera-detalle/tercera-detalle.page.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-tercera-detalle',
+  templateUrl: './tercera-detalle.page.html',
+  styleUrls: ['./tercera-detalle.page.scss'],
+})
+export class TerceraDetallePage implements OnInit {
+  personas = [
+    {
+      id    : 1,
+      nombre: 'juan',
+      edad  : 25,
+      foto  : 'https://img.freepik.com/foto-gratis/retrato-estudio-hombre-moreno-confianza_1187-5799.jpg?w=2000'
+    },
+    {
+      id    : 2,
+      nombre: 'Diana',
+      edad  : 30,
+      foto  : 'https://image.shutterstock.com/image-photo/happy-young-woman-sitting-on-260nw-2018571389.jpg'
+
+    },
+    {
+      id    : 3,
+      nombre: 'Pedro',
+      edad  : 45,
+      foto  : 'https://img.freepik.com/foto-gratis/apuesto-hombre-empresario-sonriendo-alegre_176420-17877.jpg?w=2000'
+
+    }
+  ]
+  persona: any;
+  constructor(private activatedRoute:ActivatedRoute) { }
+
+  ngOnInit() {
+    // ejercicio 16: capturar el valor del parametro y
+    // obtener la persona según id
+  }
+
+}
