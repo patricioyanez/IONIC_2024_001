690c2f2851fe52736f1455a1079c769296e71a5b
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-03 17:26:23 -0300

    Update cuarta.page.ts

--- miApp001_EA2/src/app/cuarta/cuarta.page.ts
+++ miApp001_EA2/src/app/cuarta/cuarta.page.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+
+// instalar: npm install @ionic/storage-angular
 //import { Storage } from '@ionic/storage-angular';
+// y en app.module agregar
+// import { IonicStorageModule } from '@ionic/storage-angular';
+// tambien en @NgModule({ 
+// imports: [HttpClientModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+
 
 @Component({
   selector: 'app-cuarta',
