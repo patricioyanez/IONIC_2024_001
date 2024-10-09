fa26d2c3802827e09076e8dd4d1e1865d8788622
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-09 12:56:31 -0300

    update

---
+++ miApp001_EA2/src/app/quinta/crud.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, IterableDiffers } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+
+  constructor() { }
+
+  async guardar(id:string, valor: any)
+  {
+    localStorage.setItem(id, valor);
+  }
+  async leer(id:string)
+  {
+    return localStorage.getItem(id);
+  }
+  async eliminar(id:string)
+  {
+    localStorage.removeItem(id)
+  }
+  async listar()
+  {
+    let listado : any = [];
+    for(let i=0; i < localStorage.length; i++)
+    {
+      listado[i] = localStorage.getItem("" + i);
+    }
+    return listado;
+  }
+}
