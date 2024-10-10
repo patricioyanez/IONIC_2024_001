c1ab5334673afd3af9ece2239efdc4d65adfa306
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-10 13:45:22 -0300

    update

--- miApp001_EA2/src/app/quinta/quinta.page.ts
+++ miApp001_EA2/src/app/quinta/quinta.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CrudService } from './crud.service';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-quinta',
@@ -8,15 +9,54 @@ import { CrudService } from './crud.service';
 })
 export class QuintaPage implements OnInit {
   persona:any=[];
-  constructor(private crudService:CrudService) { }
+  constructor(private crudService:CrudService,
+              private alertController:AlertController,
+              private toastController:ToastController,
+  ) { }
 
   ngOnInit() {
   }
-  guardar()
+  async guardar()
   {
     // validar si están todos los datos obligatorios.
-    this.crudService.guardar(this.persona.rut, this.persona);
-    this.limpiar();
+    if(this.persona.rut == null)
+    {
+      this.mensajeDeError('Falta especificar el rut');
+    }
+    else if(this.persona.nombre == null)
+    {
+      this.mensajeDeError('Falta especificar el nombre');
+    }
+    else if(this.persona.direccion == null)
+    {
+      this.mensajeDeError('Falta especificar la dirección');
+    }
+    else if(this.persona.correo == null)
+    {
+      this.mensajeDeError('Falta especificar el correo');
+    }
+    else if(this.persona.telefono == null)
+    {
+      this.mensajeDeError('Falta especificar el teléfono');
+    }
+    else if(this.persona.anio == null || this.persona.anio < 1900)
+    {
+      this.mensajeDeError('El año ingresado no es correcto');
+    }
+    else
+    {
+      this.crudService.guardar(this.persona.rut, this.persona);
+      const toast = await this.toastController.create({
+        header  : "Resultado",
+        message : "Datos Guardados",
+        icon    : 'checkmark-circle-outline',
+        color   : 'success',
+        duration: 2000,
+        position: 'middle'
+      });
+      await toast.present();
+      this.limpiar();
+    }
   }
   limpiar()
   {
@@ -25,4 +65,15 @@ export class QuintaPage implements OnInit {
     if(input != null)
       input.setFocus();
   }
+  async mensajeDeError(mensaje:string)
+  {
+    const alerta = await this.alertController.create({
+      header    : "Error",
+      subHeader : "Mensaje del error",
+      message   : mensaje,
+      buttons   : ['Aceptar']
+    });
+    await alerta.present();
+  }
+
 }
