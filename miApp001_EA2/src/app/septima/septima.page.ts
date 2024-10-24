10c198b9880d1a7ac20dc461170775a6c51878de
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-24 13:29:42 -0300

    update

--- miApp001_EA2/src/app/septima/septima.page.ts
+++ miApp001_EA2/src/app/septima/septima.page.ts
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./septima.page.scss'],
 })
 export class SeptimaPage implements OnInit {
+/*
+ver septima.module 
+*/
   /*
   ejercicio de investigación 28: buscar e integrar libreria para lectura de QR
   al leer, mostrar la información en un text o label.
 
 (barcodescanner)
+
+(@capacitor/barcode-scanner)
 crear octava pagina
 
 traer cable usb para conectar celular android al PC.
@@ -18,8 +23,12 @@ Descargar e instalar :
 https://www.dev47apps.com/droidcam/windows/
 pc y android
 
-
+Buscar numero de compilación (hacer 7 veces clic) y
+activar modo depuración USB
+Iniciar aplicaciones (reinicar app si esta ejecutandose)
 */
+
+
   texto : any;
   constructor() { }
 
